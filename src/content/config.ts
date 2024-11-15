510ad3f2a2196f122323dbce2f8b1ee27e28bc7e
Edwin Ortiz <edarcode@gmail.com>
2024-11-15 14:02:31 -0500

    intentando pintar imgs

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,14 +2,17 @@ import { defineCollection, z } from "astro:content";
 
 const blogCollection = defineCollection({
   type: "content",
-  schema: z.object({
-    title: z.string(),
-    date: z.date(),
-    description: z.string(),
-    author: z.string(),
-    image: z.string(),
-    tags: z.array(z.string()),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      date: z.date(),
+      description: z.string(),
+      author: z.string(),
+      image: image().refine((img) => img.width < 1200, {
+        message: "Image should be lower than 1200px",
+      }),
+      tags: z.array(z.string()),
+    }),
 });
 
 export const collection = {
